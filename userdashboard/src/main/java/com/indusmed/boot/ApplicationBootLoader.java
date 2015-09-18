5514055b5d8a721c6b9d61df36528384e4e70bbb
sandeep.kumar08@snapdeal.com <sandeep.kumar08@snapdeal.com>
2015-09-18 18:09:05 +0530

    Some minor improvements

---
+++ userdashboard/src/main/java/com/indusmed/boot/ApplicationBootLoader.java
@@ -0,0 +1,52 @@
+package com.indusmed.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.indusmed.dao.UserManagementDaoImpl;
+
+@ComponentScan("com.indusmed")
+@Configuration
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        SecurityAutoConfiguration.class })
+public class ApplicationBootLoader extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApplicationBootLoader.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBootLoader.class, args);
+	}
+	
+	@Bean
+	public DriverManagerDataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/indusmed");
+		dataSource.setUsername("root");
+		dataSource.setPassword("sandy");
+		return dataSource;
+	}
+	
+	@Bean
+	public UserManagementDaoImpl userManagementDao(){
+		UserManagementDaoImpl dao = new UserManagementDaoImpl();
+		dao.setDataSource(dataSource());		
+		return dao;
+	}
+}
